121cd28030b16e5d0ef5191a03e565623f999dc2
Antonella Domenez <domenezantonellaa@gmail.com>
2022-02-17 15:00:56 -0300

    catalogo-peliculas

---
+++ catalogo-peliculas/model/peliculas_dao.py
@@ -0,0 +1,115 @@
+from .conexion_db import ConexionDB
+from tkinter import messagebox
+
+def crear_tabla():
+    conexion = ConexionDB()
+
+    sql = '''
+    CREATE TABLE peliculas(
+        id_pelicula INTEGER,
+        nombre VARCHAR(100),
+        duracion VARCHAR(10),
+        genero VARCHAR(100),
+        PRIMARY KEY(id_pelicula AUTOINCREMENT)
+    )
+    '''
+
+    try: 
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+        titulo = 'Crear Registro'
+        mensaje = 'Se creó la tabla en la Base de datos'
+        messagebox.showinfo(titulo, mensaje)
+    except:
+        titulo = 'Crear Registro'
+        mensaje = 'La tabla ya se creó'
+        messagebox.showwarning(titulo, mensaje)
+
+def borrar_tabla():
+    conexion = ConexionDB()
+
+    sql = 'DROP TABLE peliculas'
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+        titulo = 'Borrar Registro'
+        mensaje = 'Se borró la tabla en la Base de datos'
+        messagebox.showinfo(titulo, mensaje)
+
+    except:
+        titulo = 'Borrar Registro'
+        mensaje = 'No hay tabla para borrar'
+        messagebox.showerror(titulo, mensaje)
+
+class Pelicula:
+    def __init__(self, nombre, duracion, genero):
+        self.id_pelicula = None
+        self.nombre = nombre
+        self.duracion = duracion
+        self.genero = genero
+
+    def __str__(self):
+        return f'Pelicula[{self.nombre}, {self.duracion}, {self.genero}]'
+
+def guardar(pelicula):
+    conexion = ConexionDB()
+
+    sql = f'''INSERT INTO peliculas (nombre, duracion, genero)
+    VALUES("{pelicula.nombre}", "{pelicula.duracion}", "{pelicula.genero}")'''
+
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+
+    except:
+        titulo = 'Conexión al Registro'
+        mensaje = 'La tabla peliculas no está creada en la base de datos'
+        messagebox.showerror(titulo, mensaje)
+ 
+
+def listar():
+    conexion = ConexionDB()
+
+    lista_peliculas = []
+    sql = 'SELECT * FROM peliculas'
+
+    try:
+        conexion.cursor.execute(sql)
+        lista_peliculas = conexion.cursor.fetchall()
+        conexion.cerrar()
+    except:
+        titulo = 'Conexión al Registro'
+        mensaje = 'Crea la tabla en la base de datos'
+        messagebox.showwarning(titulo, mensaje)
+
+    return lista_peliculas
+
+def editar(pelicula, id_pelicula):
+    conexion = ConexionDB()
+
+    sql = f"""UPDATE peliculas 
+    SET nombre = '{pelicula.nombre}', duracion = '{pelicula.duracion}',
+    genero = '{pelicula.genero}'
+    WHERE id_pelicula = '{id_pelicula}'"""
+
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+
+    except:
+        titulo = 'Editar datos'
+        mensaje = 'Error para editar el registro'
+        messagebox.showerror(titulo, mensaje)
+
+def eliminar(id_pelicula):
+    conexion = ConexionDB()
+    sql = f'DELETE FROM peliculas WHERE id_pelicula = {id_pelicula}'
+
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+
+    except:
+        titulo = 'Eliminar datos'
+        mensaje = 'Error para eliminar el registro'
+        messagebox.showerror(titulo, mensaje)
